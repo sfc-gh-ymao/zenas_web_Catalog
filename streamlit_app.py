2f088e003297917ea9c17c71bddbf3dc2bb4fab9
Yun Mao <94030617+sfc-gh-ymao@users.noreply.github.com>
2023-06-24 09:55:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('Zenas Web Catalog')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets[snowflake])
 my_cur = my_cnx.cursor()
 my_cur.execute('SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()')
 my_data_row = my_cur.fetchone()
