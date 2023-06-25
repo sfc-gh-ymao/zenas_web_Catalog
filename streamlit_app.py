c7e5752f56c921d727bd8d54d889c31b28ac7f43
Yun Mao <94030617+sfc-gh-ymao@users.noreply.github.com>
2023-06-24 22:15:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLError
+from urllib.error 
+import URLError
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # connect to snowflake
